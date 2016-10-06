2040175ba29cdb35a6b23229248d1362cd36f956
hydrargyrum <dev@indigo.re>
2016-10-06 08:56:05 +0200

    add a test image and a path test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import md5
 import os
 import shutil
 import tempfile
@@ -12,11 +13,16 @@ import thumbnail
 class ThumbnailTests(unittest.TestCase):
 	def setUp(self):
 		self.dir = os.environ['XDG_CACHE_HOME'] = tempfile.mkdtemp()
-		self.filename = os.path.join(os.environ['XDG_CACHE_HOME'], 'r.png')
-		shutil.copyfile('r.png', self.filename)
+		self.filename = os.path.join(os.environ['XDG_CACHE_HOME'], 'test.png')
+		shutil.copyfile('test.png', self.filename)
 
 	def tearDown(self):
 		shutil.rmtree(self.dir)
+
+	def test_hash(self):
+		uri = 'file://%s' % self.filename
+		dest = os.path.join(self.dir, 'thumbnails', 'large', md5.new(uri).hexdigest()) + '.png'
+		self.assertEqual(dest, thumbnail.build_thumbnail_path(self.filename, 'large'))
 
 	def test_basic(self):
 		dest = thumbnail.build_thumbnail_path(self.filename, 'large')
